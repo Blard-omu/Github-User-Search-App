cc7168afadbefd12193b8898cf148dce35c25fd9
BLARD <102221908+Blard-omu@users.noreply.github.com>
2023-06-23 04:34:37 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Header = (props) => {
 const { title = "Welcome to Techstudio", desc = "You can become a tech pro in just one week"} = props
   return (
-    <div>
-        <div className="container bg-light m-4 p-3 col-md-8 offset-md-2 shadow-lg text-center">
+    <>
+        <div className="container bg-light m-4 p-3 col-md-8 offset-md-4 shadow-lg text-center">
             <h1 className='bg-primary text-light p-3'>{title}</h1>
             <p className='bg-info text-dark p-2 mx-4'>{desc}</p>
 
         </div>
-    </div>
+    <>
   )
 }
 
