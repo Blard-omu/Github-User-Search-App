7b54bdad0b0dff5e21d2dd64e1e051142dd6b4a7
BLARD <102221908+Blard-omu@users.noreply.github.com>
2023-06-23 17:04:45 +0100

    Update Header.jsx
    Header formatted

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,6 @@ const { title = "Welcome to Techstudio", desc = "You can become a tech pro"} = p
         <div className="container bg-light m-4 p-3 col-md-6 offset-md-3 shadow-lg text-center">
             <h1 className='bg-primary text-light p-3'>{title}</h1>
             <p className='bg-info text-dark p-2 mx-4'>{desc}</p>
-
         </div>
     <>
   )
