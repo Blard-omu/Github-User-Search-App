83c4a86d803bd17f6360d8c6b270c339f7a7e394
BLARD <102221908+Blard-omu@users.noreply.github.com>
2023-12-01 11:48:43 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = (props) => {
 const { title = "Welcome to Techstudio", desc = "You can become a tech pro"} = props
   return (
     <>
-        <div className="container bg-light m-4 p-3 col-md-6 offset-md-3 shadow-lg text-center">
+        <div className=" bg-light m-4 p-3 col-md-6 offset-md-3 shadow-lg text-center">
             <h1 className='bg-primary text-light p-3'>{title}</h1>
             <p className='bg-info text-dark p-2 mx-4'>{desc}</p>
         </div>
